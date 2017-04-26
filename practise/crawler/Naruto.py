47c9b70f51e24d80d8e65e05a1d240e8532c25ad
wangbinbin <2404016117@qq.com>
2017-04-26 19:10:18 +0800

    p

--- practise/crawler/Naruto.py
+++ practise/crawler/Naruto.py
@@ -2,6 +2,7 @@
 #coding:utf-8
 import os 
 import re
+import json
 import urllib2
 import decodeM
 from bs4 import BeautifulSoup
@@ -13,72 +14,75 @@ UTF8Writer = codecs.getwriter('utf8')
 sys.stdout = UTF8Writer(sys.stdout)
 
 
-
-
 class naruto:
 
-	def __init__(self,url):
+    def __init__(self,url):
 
-		soup = self.getHtml(url,True)
-		chapters = soup.find_all('ol', class_='chapter-page-all')[0].find_all('span',class_='works-chapter-item')
+        soup = self.getHtml(url,True)
+        chapters = soup.find_all('ol', class_='chapter-page-all')[0].find_all('span',class_='works-chapter-item')
 
-		index = 1
+        index = 1
 
-		for i in chapters :
-			try:
-				file_path = 'img/'+i.a['title']
-				# os.mkdir(file_path)
-				htmlStr = self.getHtml('http://ac.qq.com'+i.a['href'])
+        for i in chapters :   
+            try:
+                file_path = 'img/'+i.a['title']
+                # os.mkdir(file_path)
+                htmlStr = self.getHtml('http://ac.qq.com'+i.a['href'])
 
-				if index == 1 :				
-					index = 2
-					data = re.search(r'\'\S{200,}\'',htmlStr).group()
+                if index == 1 :             
+                    index = 2
 
-					print '------------------------------'
+                    data = re.search(r'\'\S{200,}\'',htmlStr).group()[1:-1]
 
-					getData = decodeM.decode(data)
+                    # getData = decodeM.decode(data[1:])
 
-					print type(getData)
-					print getData
+                    json_data = json.loads(decodeM.decode(data[1:]))
+                  
 
-					var = getData.encode('utf-8')
+                    # for j in json_data['picture']:
+                    #     print j['url']
 
-					with open('demo.txt','wb') as fp:
-
-						fp.write(data)
-
-					# print decodeM.decode(data) 
-
-			except Exception,e:
-				print '**************************************'
-				print Exception,':',e
-
-	def getHtml(self,url,flag=False):
-
-		request = urllib2.Request(url)
-		response = urllib2.urlopen(request)
-		html = response.read()
-		if flag == True:
-			html = BeautifulSoup(html)
-		return html
+                    for j,n in enumerate(json_data['picture']):
+                        print j
+                        print n['url']
 
 
-	@staticmethod
-	def download(url,save_path):
+                    # with open('demo.txt','wb') as fp:
 
-		imgData = urllib2.urlopen(url).read()
+                    #     fp.write(getData)
 
-		with open(save_path,'wb') as fp:
-			fp.write(imgData)
+                    # print decodeM.decode(data) 
+
+            except Exception,e:
+                print '**************************************'
+                print Exception,':',e
+
+    def getHtml(self,url,flag=False):
+
+        request = urllib2.Request(url)
+        response = urllib2.urlopen(request)
+        html = response.read()
+        if flag == True:
+            html = BeautifulSoup(html)
+        return html
+
+
+    @staticmethod
+    def download(url,save_path):
+
+        imgData = urllib2.urlopen(url).read()
+
+        with open(save_path,'wb') as fp:
+            fp.write(imgData)
 
 
 if __name__ == '__main__':
 
-	# url = 'http://ac.tc.qq.com/store_file_download?buid=15017&uin=1468625909&dir_path=/&name=16_07_38_51ce7aa442438201fa8d052de6fcfaf4_3193.jpg'
-	# save_path = 'img/2.jpg'
+    # url = 'http://ac.tc.qq.com/store_file_download?buid=15017&uin=1468625909&dir_path=/&name=16_07_38_51ce7aa442438201fa8d052de6fcfaf4_3193.jpg'
+    # save_path = 'img/2.jpg'
 
-	demo = naruto('http://ac.qq.com/Comic/comicInfo/id/544907')
-	# demo = naruto('http://ac.qq.com/ComicView/index/id/544907/cid/24')
+    demo = naruto('http://ac.qq.com/Comic/comicInfo/id/544907')
+    # demo = naruto('http://ac.qq.com/ComicView/index/id/544907/cid/24')
 
 
 
