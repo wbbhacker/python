d470a2685f6b00aab65a0059cc4696feb51658de
wangbinbin <2404016117@qq.com>
2017-04-28 19:11:08 +0800

    p

---
+++ practise/mysite/learn/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+	a = request.GET['a']
+	b = request.GET.get('b',10)
+	c = int(a) + int(b)
+	return HttpResponse(c)
+
+
+def detail(request,a,b):
+	c = int(a) + int(b)
+	return HttpResponse(str(c))
+
+
+def login(request):
+	return render(request,'login.html')
