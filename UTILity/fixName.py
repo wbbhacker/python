5c362b9d6376d7adb21b1e37efdfc8ffabcf70cd
wbbhacker <2404016117@qq.com>
2017-09-01 10:51:47 +0800

    add

---
+++ UTILity/fixName.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import os
+
+def fixName(path,name):
+
+	for root, dirs, files in os.walk(path):
+		
+		for i in range(len(	files)):
+			ext = files[i].split('.')[-1]
+
+			orgName = path + '/' + files[i]
+			newName = path + '/'+name+str(i)+'.'+ext
+			os.rename(orgName,newName)
+
+if __name__ == '__main__':
+	fixName('kv-wen','kv-wen')
